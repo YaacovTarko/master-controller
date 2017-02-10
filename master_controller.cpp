323a43fb64d43646a91f5ae16e09fffd6f4b2264
Yaacov Tarko <yaacov.tarko@gmail.com>
2017-02-09 20:01:17 -0800

    Finished porting to C++

--- master_controller.cpp
+++ master_controller.cpp
@@ -1,24 +1,21 @@
-#include <errno.h>      //for perror
-#include <stdio.h>      //for perror and other i/o
-#include <stdlib.h>     //for memory management (malloc, free...)
-#include <string.h>     //for strcpy, strlen, etc...
-#include <sys/socket.h> //for socket
-#include <sys/types.h>  //for getpid, connect, ...
-#include <sys/stat.h> //for open
-#include <unistd.h> //for close, and other stuff too but I'm not sure what
-#include <fcntl.h> //for open
-#include <string> 
+#include <errno.h>  //for perror
+#include <stdio.h>  //for perror and other i/o
+#include <string>
+#include <unistd.h>     //for fork, pipe, execvp, dup2
 
-
-void launch_process(char *path, char *const args[]) {
+void launch_process(std::string path, std::string args, int *input_pipe,
+                    int *output_pipe) {
   pid_t process_id = fork();
   if (process_id == -1) {
     perror("Fork failed in create_process");
   }
   if (process_id == 0) { // if this is the child process
+  	//Set the pipes through which the child process will communicate with this program as stdio
+  	dup2(*input_pipe, 0);
+  	dup2(*output_pipe, 1); 
+
     if (execvp(path, args) == -1) {
       perror("Exec failed in create_process");
-      return;
     }
   }
   return;
@@ -26,34 +23,31 @@ void launch_process(char *path, char *const args[]) {
 
 // creates pipe for interprocess communication. Won't work with execvp
 int *create_pipe() {
-  int* file_designators = new int[2];
+  int *file_designators = new int[2];
   if (!pipe(file_designators)) {
     perror("Pipe failed in create_pipe");
   }
   return file_designators;
 }
 
-
 int main(int argc, char *argv[]) {
-	//create pipes to pass data between this process and its children
-	int* pipe_to_mission_planner = create_pipe();  
-	int* pipe_from_mission_planner = create_pipe();
-	int* pipe_to_interop_system = create_pipe();
-	int* pipe_from_interop_system = create_pipe(); 
+  // create pipes to pass data between this process and its children
+  int *pipe_to_mission_planner = create_pipe();
+  int *pipe_from_mission_planner = create_pipe();
+  int *pipe_to_interop_system = create_pipe();
+  int *pipe_from_interop_system = create_pipe();
   // these can be used to pass args to the client
-  char * interop_client_args;
-  char * mission_planner_args;
+  std::string interop_client_args;
+  std::string mission_planner_args;
 
   // create new processes for the interop client system and mission planner
-  // launch_process("pathname_of_interop_client", interop_client_args);
-  // launch_process("pathname_of_mission_planner", mission_planner_args);
+  // launch_process("pathname_of_interop_client", interop_client_args, pipe_to_mission_planner[0], pipe_from_mission_planner[1]);
+  // launch_process("pathname_of_mission_planner", mission_planner_args, pipe_to_interop_system[0], pipe_from_interop_system[1]);
 
-
-
-//clean up and exit
+  // clean up and exit
   delete pipe_to_interop_system;
   delete pipe_from_interop_system;
   delete pipe_to_mission_planner;
-  delete pipe_from_mission_planner; 
+  delete pipe_from_mission_planner;
   return 0;
 }
